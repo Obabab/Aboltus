8b796a529b080d5f39176298e1807f201944575b
Qwerty333-cel <53406518+Qwerty333-cel@users.noreply.github.com>
2025-03-29 07:07:35 +0400

    Docker start added

--- proj_setup.bat
+++ proj_setup.bat
@@ -4,25 +4,29 @@ cd /d %~dp0
 :: 0. Освобождаем порт 5432 для работы базы данных
 call port_clearer.bat
 
-:: 1. Создаём виртуальное окружение
+:: 1. Запускаем Docker Desktop
+start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+timeout /t 20 /nobreak
+
+:: 2. Создаём виртуальное окружение
 python -m venv venv
 
-:: 2. Активируем виртуальное окружение
+:: 3. Активируем виртуальное окружение
 call venv\Scripts\activate
 
-:: 3. Запускаем Docker Compose
+:: 4. Запускаем Docker
 docker-compose up -d
 
-:: 4. Устанавливаем зависимости
+:: 5. Устанавливаем зависимости
 pip install -r requirements.txt
 
-:: 5. Генерируем миграцию
+:: 6. Генерируем миграцию
 alembic revision --autogenerate -m "add new table"
 
-:: 6. Применяем миграции
+:: 7. Применяем миграции
 alembic upgrade head
 
-:: 7. Завершаем скрипт
+:: 8. Завершаем скрипт
 echo.
 echo [INFO] Проект настроен и готов к работе!
 cmd /k
